e70371520f2dc0becf77acc5d287a6f4eeebe92d
Ahlam Raiteb <araiteb@e2r13p1.1337.ma>
2024-01-14 14:40:26 +0100

    wordpress script

--- incption/srcs/requirements/wordpress/tools/script.sh
+++ incption/srcs/requirements/wordpress/tools/script.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+echo "hey !!" 
 mkdir /var/www
 mkdir /var/www/html
+CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE USER 'yumee'@'%' IDENTIFIED WITH mysql_native_password BY 'yume';
+GRANT ALL ON wordpress.* TO 'wordpressuser'@'%';
+apt update;
+apt install -y php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip;
